c341f0ce569f73e27fdcf08fb959ffb7999dd97e
Shark <Shark@BOHC49A87.admin.bvsd.k12.co.us>
2017-01-19 16:55:29 -0700

    Autonomous Arm1 command, moves it up

--- SteamMeme/src/org/usfirst/frc/team1157/robot/Robot.java
+++ SteamMeme/src/org/usfirst/frc/team1157/robot/Robot.java
@@ -7,6 +7,7 @@ import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.usfirst.frc.team1157.robot.commands.ExampleCommand;
+import org.usfirst.frc.team1157.robot.commands.MoveArm1Up;
 import org.usfirst.frc.team1157.robot.subsystems.Arm1;
 import org.usfirst.frc.team1157.robot.subsystems.ExampleSubsystem;
 
@@ -35,6 +36,7 @@ public class Robot extends IterativeRobot {
     public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
     
     Command autonomousCommand;
+    Command autonomousArm1Command;
     SendableChooser<Command> chooser = new SendableChooser<>();
 
     private static final int IMG_WIDTH = 1080;
@@ -53,7 +55,7 @@ public class Robot extends IterativeRobot {
     public void robotInit() {
 	oi = new OI();
 	chooser.addDefault("Default Auto", new ExampleCommand());
-	// chooser.addObject("My Auto", new MyAutoCommand());
+	chooser.addObject("arm 1", new MoveArm1Up());
 	SmartDashboard.putData("Auto mode", chooser);
 	UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
 	//camera.setResolution(IMG_WIDTH, IMG_HEIGHT);
@@ -148,3 +150,4 @@ public class Robot extends IterativeRobot {
 	LiveWindow.run();
     }
 }
+
