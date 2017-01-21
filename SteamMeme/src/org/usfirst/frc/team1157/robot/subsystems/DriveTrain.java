dc4e37c1652751088c8544720e0ef7fc0c393b14
Shark <Shark@BOHC49A87.admin.bvsd.k12.co.us>
2017-01-21 12:16:06 -0700

    Switched DriveTrain motors to CANTalons.

--- SteamMeme/src/org/usfirst/frc/team1157/robot/subsystems/DriveTrain.java
+++ SteamMeme/src/org/usfirst/frc/team1157/robot/subsystems/DriveTrain.java
@@ -1,11 +1,12 @@
 package org.usfirst.frc.team1157.robot.subsystems;
+import com.ctre.CANTalon;
 
+import org.usfirst.frc.team1157.robot.Robot;
 import org.usfirst.frc.team1157.robot.RobotMap;
 import org.usfirst.frc.team1157.robot.commands.DriveTrainJoystickDrive;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -16,18 +17,18 @@ public class DriveTrain extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
-	Talon frontRightMotor;
-	Talon frontLeftMotor;
-	Talon backRightMotor;
-	Talon backLeftMotor;
+	CANTalon frontRightMotor;
+	CANTalon frontLeftMotor;
+	CANTalon backRightMotor;
+	CANTalon backLeftMotor;
 	RobotDrive robotDrive;
 	
 	
 	public DriveTrain() {
-    	frontRightMotor = new Talon(RobotMap.frontRightMotor);
-    	frontLeftMotor = new Talon(RobotMap.frontLeftMotor);
-    	backRightMotor = new Talon(RobotMap.backRightMotor);
-    	backLeftMotor = new Talon(RobotMap.backLeftMotor);
+    	frontRightMotor = new CANTalon(RobotMap.frontRightMotor);
+    	frontLeftMotor = new CANTalon(RobotMap.frontLeftMotor);
+    	backRightMotor = new CANTalon(RobotMap.backRightMotor);
+    	backLeftMotor = new CANTalon(RobotMap.backLeftMotor);
     	robotDrive = new RobotDrive(frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor);
     	
 	
@@ -43,7 +44,7 @@ public class DriveTrain extends Subsystem {
         //setDefaultCommand(new MySpecialCommand());
     }
     public void driveWithJoystick(Joystick joystick) {
-    	robotDrive.mecanumDrive_Cartesian(joystick.getX(), joystick.getY(), joystick.getTwist(), 0);
+    	robotDrive.mecanumDrive_Cartesian(joystick.getX(), joystick.getY(), joystick.getTwist(), Robot.gyro.getAngle());
     	
     	
     }
