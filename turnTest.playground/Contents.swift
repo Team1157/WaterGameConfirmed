3c2ab84311b98da8cd10b1eb8b167690e8f0954f
Doodleman360 <julislo123@gmail.com>
2017-02-21 16:43:52 -0700

    modified playground

--- turnTest.playground/Contents.swift
+++ turnTest.playground/Contents.swift
@@ -1,9 +1,9 @@
-var Kp = 0.45;
+var Kp = 0.6;
 var finished = false;
 var angle = 60.0;
-var gyroAngle = 60.0;
+var gyroAngle = 0.0;
 
-var error = (angle - gyroAngle)/15.0;
+var error = (angle - gyroAngle)/90.0;
 var setSpeed = Kp * (error);
     
 if (abs(gyroAngle - angle) >= 2.5){
